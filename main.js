5648952d5be0c7a761cfb85cf6712c42b4395e41
Adam Hassan <adamhassan@pm.me>
2020-09-16 00:10:35 +0200

    Update main.js
    Refactoring, and cleanup

--- main.js
+++ main.js
@@ -9,57 +9,77 @@
 
     const lastChangedSpan = document.querySelector(".date-updated");
 
+    var entries = [];
+
     //
     // Methods
     //
     function onEntrySubmit(event) {
         event.preventDefault();
 
-        // Create a single entry element from text in input textbox
-        const displayEntryBtn = document.createElement("button");
-        displayEntryBtn.className = "display-entry-button";
-        displayEntryBtn.innerText = getCurrentDateTime();
-        entriesList.appendChild(displayEntryBtn);
+        addNewEntry(entryTitle.value, entryTextbox.value);
+        updateEntries();
+        clearInputFields();
 
-        const singleEntryTitleEl = document.createElement("h3");
-        singleEntryTitleEl.className = "single-entry title"
-        singleEntryTitleEl.innerText = entryTitle.value;
-        singleEntryTitleEl.style.display = "none";
-        entriesList.appendChild(singleEntryTitleEl);
-
-        const singleEntryTextEl = document.createElement("div");
-        singleEntryTextEl.className = "single-entry clear";
-        singleEntryTextEl.innerText = entryTextbox.value
-        singleEntryTextEl.style.display = "none";
-        entriesList.appendChild(singleEntryTextEl);
-
-        count++;
-
-        displayEntryBtn.addEventListener("click", function() {
-
-            // Hide all entries upon display entry button click
-            const allEntries = document.querySelectorAll(".single-entry");
-            allEntries.forEach(element => {
-                element.style.display = "none";
-            });
-
-            // Only show currently pressed entry
-            singleEntryTitleEl.style.display = "block";
-            singleEntryTextEl.style.display = "block";
-        });
-
-        updateLastChanged();
-        clearFields();
+        console.log(entries);
     }
 
+    // Add entry
+    function addNewEntry(title, description) {
+        let entry = {
+            entryTitle: title,
+            entryDescription: description,
+            entryDate: getCurrentDateTime()
+        }
 
+        entries.push(entry);   
+    }
+
+    function updateEntries() {
+        // Clear out entires from list in html dom 
+        entriesList.innerHTML = "";
+
+        entries.forEach(entry => {
+            const displayEntryBtn = document.createElement("button");
+            displayEntryBtn.className = "display-entry-button";
+            displayEntryBtn.innerText = entry.entryDate;
+            entriesList.insertAdjacentElement('afterbegin', displayEntryBtn); // Insert at the start of entires
+
+            const singleEntryTitleEl = document.createElement("h3");
+            singleEntryTitleEl.className = "single-entry title"
+            singleEntryTitleEl.innerText = entry.entryTitle;
+            singleEntryTitleEl.style.display = "none";
+            entriesList.insertAdjacentElement('afterbegin', singleEntryTitleEl);
+
+            const singleEntryTextEl = document.createElement("div");
+            singleEntryTextEl.className = "single-entry clear";
+            singleEntryTextEl.innerText = entry.entryDescription
+            singleEntryTextEl.style.display = "none";
+            entriesList.insertAdjacentElement('afterbegin', singleEntryTextEl);
+
+            displayEntryBtn.addEventListener("click", function() {
+
+                // Hide all entries upon display entry button click
+                const allEntries = document.querySelectorAll(".single-entry");
+                allEntries.forEach(element => {
+                    element.style.display = "none";
+                });
+    
+                // Only show currently pressed entry
+                singleEntryTitleEl.style.display = "block";
+                singleEntryTextEl.style.display = "block";
+            });
+        })
+
+        updateLastChangedDate();
+    }
 
     // Updates last changed date span 
-    function updateLastChanged() {
+    function updateLastChangedDate() {
         lastChangedSpan.textContent = "Last updated: " + getCurrentDateTime();
     }
 
-    function clearFields() {
+    function clearInputFields() {
         entryTitle.value = "";
         entryTextbox.value = "";
     }
