ad909674d306dc22e5a7330363a3c13aff056bb4
Adam Hassan <adamhassan@pm.me>
2020-09-15 20:49:54 +0200

    Improved entries list
    Added show/hide toggle for entries
    Refactoring to js code and css classes

--- main.js
+++ main.js
@@ -2,10 +2,12 @@
     //
     // Variables
     //
-    const entryForm = document.getElementById("entry-form");
-    const entryTextbox = document.getElementsByClassName("entry-textbox")[0];
-    const entriesSection = document.getElementById("entries-section");
-    const lastChangedSpan = document.getElementsByClassName("date-updated")[0];
+    const entryForm = document.querySelector("#entry-form");
+    const entriesList = document.querySelector(".entries-list");
+    const entryTitle = document.querySelector(".entry-title");
+    const entryTextbox = document.querySelector(".entry-textbox");
+
+    const lastChangedSpan = document.querySelector(".date-updated");
 
     //
     // Methods
@@ -14,14 +16,56 @@
         event.preventDefault();
 
         // Create a single entry element from text in input textbox
-        const singleEntryEl = document.createElement("div");
-        singleEntryEl.className = "single-entry clear";
-        singleEntryEl.innerText = entryTextbox.value
-        entriesSection.appendChild(singleEntryEl);
+        const displayEntryBtn = document.createElement("button");
+        displayEntryBtn.className = "display-entry-button";
+        displayEntryBtn.innerText = getCurrentDateTime();
+        entriesList.appendChild(displayEntryBtn);
+
+        const singleEntryTitleEl = document.createElement("h3");
+        singleEntryTitleEl.className = "single-entry title"
+        singleEntryTitleEl.innerText = entryTitle.value;
+        singleEntryTitleEl.style.display = "none";
+        entriesList.appendChild(singleEntryTitleEl);
+
+        const singleEntryTextEl = document.createElement("div");
+        singleEntryTextEl.className = "single-entry clear";
+        singleEntryTextEl.innerText = entryTextbox.value
+        singleEntryTextEl.style.display = "none";
+        entriesList.appendChild(singleEntryTextEl);
+
+        count++;
+
+        displayEntryBtn.addEventListener("click", function() {
+
+            // Hide all entries upon display entry button click
+            const allEntries = document.querySelectorAll(".single-entry");
+            allEntries.forEach(element => {
+                element.style.display = "none";
+            });
+
+            // Only show currently pressed entry
+            singleEntryTitleEl.style.display = "block";
+            singleEntryTextEl.style.display = "block";
+        });
+
+        updateLastChanged();
+        clearFields();
     }
+
+
 
     // Updates last changed date span 
     function updateLastChanged() {
+        lastChangedSpan.textContent = "Last updated: " + getCurrentDateTime();
+    }
+
+    function clearFields() {
+        entryTitle.value = "";
+        entryTextbox.value = "";
+    }
+
+    // Gets current date time
+    function getCurrentDateTime() {
         let nowDate = new Date();
         nowDate.toLocaleDateString('en-US', {
             month: 'long',
@@ -30,7 +74,7 @@
             hour: '2-digit',
         });
 
-        lastChangedSpan.textContent += nowDate.toLocaleString();
+        return nowDate.toLocaleString();
     }
 
     //
