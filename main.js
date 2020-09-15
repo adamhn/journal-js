6e62b5c97bec7eff42f9de47e0b302b9aac27132
Adam Hassan <adamhassan@pm.me>
2020-09-15 12:50:33 +0200

    Styling entries section Structure js file Add date method for last updated

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
-const entryForm = document.getElementById("entry-form");
-const entryTextbox = document.getElementsByClassName("entry-textbox")[0];
-const entriesSection = document.getElementById("entries-section");
+(function() {
+    //
+    // Variables
+    //
+    const entryForm = document.getElementById("entry-form");
+    const entryTextbox = document.getElementsByClassName("entry-textbox")[0];
+    const entriesSection = document.getElementById("entries-section");
+    const lastChangedSpan = document.getElementsByClassName("date-updated")[0];
 
-function onEntrySubmit(event) {
-    event.preventDefault();
+    //
+    // Methods
+    //
+    function onEntrySubmit(event) {
+        event.preventDefault();
 
-    // Create a single entry element from text in input textbox
-    const singleEntryEl = document.createElement("div");
-    singleEntryEl.className = "single-entry clear";
-    singleEntryEl.innerText = entryTextbox.value
-    entriesSection.appendChild(singleEntryEl);
-}
+        // Create a single entry element from text in input textbox
+        const singleEntryEl = document.createElement("div");
+        singleEntryEl.className = "single-entry clear";
+        singleEntryEl.innerText = entryTextbox.value
+        entriesSection.appendChild(singleEntryEl);
+    }
 
-entryForm.addEventListener("submit", onEntrySubmit);
+    // Updates last changed date span 
+    function updateLastChanged() {
+        let nowDate = new Date();
+        nowDate.toLocaleDateString('en-US', {
+            month: 'long',
+            day: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+        });
+
+        lastChangedSpan.textContent += nowDate.toLocaleString();
+    }
+
+    //
+    // Inits & Event Listeners
+    //
+    entryForm.addEventListener("submit", onEntrySubmit);
+})();
+
 
